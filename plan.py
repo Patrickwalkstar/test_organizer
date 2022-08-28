d0379c09867a0ee3c3ffa0516dcdba4421e4566f
Patrick Lee Walker <pwalker@sandiego.edu>
2022-08-27 17:31:51 -0700

    Reorder plan file

--- plan.py
+++ plan.py
@@ -1,48 +1,20 @@
 from test import AllTestSteps, AllTests, TestSteps, TestStep, Test
 from rich import print
+from collections import OrderedDict
 
-
-
-class Plan: 
+class Plan(OrderedDict): 
     def __init__(self) -> None:
         self.stepExecutionOrder = []
         
-allTestSteps = AllTestSteps('test_steps.csv')
-allTestStepsStore = allTestSteps.test_steps_store
-allTestStepStore = allTestSteps.test_step_store
-allTestPreconditions = allTestSteps.test_preconditions
-
-stepCollection = {}
-for count, (key, value) in enumerate(allTestStepStore.items(), start=1): 
-    print(f"{count} = {key}:{value}")
-    stepCollection[count] = value
-    
-    
-for count, (key, value) in enumerate(allTestStepsStore.items(), start=1):
-    print(f"{count} = {key}:{value}")
-
-for precondition in allTestPreconditions:
-    print(precondition.TestStepID)
-
-    
-    
-allTests = AllTests('tests.csv').allTests
-for test in allTests:
-    try:
-
-        test.TestSteps = allTestStepsStore[test.TestStepID.strip()]
-        # print(test.TestSteps)
-        test.TestStepIDs = [testStep.TestStepID for testStep in test.TestSteps.asList()]
-        # print(test.TestStepIDs)
-        test.printSteps()
-    except Exception:
-        print(f'{test} didnt work')
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self.move_to_end(key)
         
+    def writePlan(self, filename: str):
+        with open(filename, 'w') as writeFile: 
+             writeFile.writelines(self.stepExecutionOrder)
         
-
-
-def main():
-    finalTestPlan = Plan()
+def main(finalTestPlan: Plan):
     
     #Calculate the number of tests or test steps that rely on each precondition
     #Based on the number of Machines that are available to the user
@@ -59,9 +31,36 @@ def main():
     #Process the remaining step lists form the stepCollection[count] = value
     #Sort  
     
-   
+    allTestSteps = AllTestSteps('test_steps.csv')
+    allTests = AllTests('tests.csv').allTests
+    allTestStepsStore = allTestSteps.test_steps_store
+    allTestStepStore = allTestSteps.test_step_store
+    allTestPreconditions = allTestSteps.test_preconditions
+
+    stepCollection = {}
+    for count, (key, value) in enumerate(allTestStepStore.items(), start=1): 
+        print(f"{count} = {key}:{value}")
+        stepCollection[count] = value
+        
+    for count, (key, value) in enumerate(allTestStepsStore.items(), start=1):
+        print(f"{count} = {key}:{value}")
+
+    for precondition in allTestPreconditions:
+        print(precondition.TestStepID)
+
     
-    pass
+    for test in allTests:
+        try:
+            test.TestSteps = allTestStepsStore[test.TestStepID.strip()]
+            # print(test.TestSteps)
+            test.TestStepIDs = [testStep.TestStepID for testStep in test.TestSteps.asList()]
+            # print(test.TestStepIDs)
+            # test.printSteps()
+        except Exception:
+            print(f'{test} didnt work')
+            
+    finalTestPlan.writePlan()
 
 if __name__ == "__main__":
-    main()
+    finalTestPlan = Plan()
+    main(finalTestPlan)
